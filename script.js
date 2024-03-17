333a95b20643fe1360665da2edeb5ea1bda69804
JeremiahDMoore <jdmenterprises@gmail.com>
2024-03-17 13:32:18 -0700

    shortened copy

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     cards.forEach(card => {
         card.addEventListener('mouseover', () => {
             card.style.transform = 'scale(1.05)';
+
         });
         card.addEventListener('mouseout', () => {
             card.style.transform = 'scale(1)';
